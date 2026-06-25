24666b17792f1e7a1cdc6bb732417f6d233aabf9
manivannancsmca <manivannancsmca@gmail.com>
2026-06-25 10:47:08 +0530

    first commit

---
+++ src/main/java/com/eks_kafka_producer_app/EksKafkaProducerAppApplication.java
@@ -0,0 +1,13 @@
+package com.eks_kafka_producer_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksKafkaProducerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksKafkaProducerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eks_kafka_producer_app/EksKafkaProducerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eks_kafka_producer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksKafkaProducerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
