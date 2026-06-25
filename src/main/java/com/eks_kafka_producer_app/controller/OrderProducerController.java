0ade7f8d58480739946039c781839c1ce22eae9e
manivannancsmca <manivannancsmca@gmail.com>
2026-06-25 11:11:19 +0530

    controller file added for produce message to kafka topic

---
+++ src/main/java/com/eks_kafka_producer_app/controller/OrderProducerController.java
@@ -0,0 +1,25 @@
+package com.eks_kafka_producer_app.controller;
+
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import com.eks_kafka_producer_app.model.OrderEvent;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderProducerController {
+
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
+    private static final String TOPIC = "orders-topic";
+
+    public OrderProducerController(KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping
+    public String publishOrder(@RequestBody OrderEvent order) {
+        kafkaTemplate.send(TOPIC, order.orderId(), order);
+        return "Order Event Published Successfully!";
+    }
+}
